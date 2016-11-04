2abba657d215bd2c51090ad0c9ae07d41b37a4db
bonino <dario.bonino@gmail.com>
2016-11-04 14:50:49 +0100

    Amended copyright notice

--- it.polito.elite.dog.drivers.modbus.network/src/it/polito/elite/dog/drivers/modbus/network/ModbusDriverImpl.java
+++ it.polito.elite.dog.drivers.modbus.network/src/it/polito/elite/dog/drivers/modbus/network/ModbusDriverImpl.java
@@ -1,7 +1,7 @@
 /*
  * Dog - Network Driver
  * 
- * Copyright (c) 2012-2013 Dario Bonino
+ * Copyright (c) 2012-2013 Dario Bonino, Claudio Degioanni
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
